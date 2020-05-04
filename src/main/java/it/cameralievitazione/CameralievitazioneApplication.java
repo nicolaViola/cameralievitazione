f40c89d462791b82606a7206d1366d2bb882e353
Viola_N <nicola.viola@regione.emilia-romagna.it>
2020-05-04 14:42:53 +0200

    aggiungi logger

--- src/main/java/it/cameralievitazione/CameralievitazioneApplication.java
+++ src/main/java/it/cameralievitazione/CameralievitazioneApplication.java
@@ -1,5 +1,7 @@
 package it.cameralievitazione;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class CameralievitazioneApplication {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(CameralievitazioneApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CameralievitazioneApplication.class, args);
@@ -15,6 +19,7 @@ public class CameralievitazioneApplication {
 	
 	@GetMapping
 	public String hi() {
+		LOGGER.debug("dentro");
 		return "HI2";
 	}
 
